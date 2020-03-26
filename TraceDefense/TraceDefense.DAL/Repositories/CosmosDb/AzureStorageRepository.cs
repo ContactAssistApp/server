686de4ab0441ba7f6c2e79cb7bf42dcc8c6bd4cb
Cory Gehr <cogehr@microsoft.com>
2020-03-26 16:14:44 -0700

    Add Azure Storage Account repository base Add QueryRecord objects Start repository for QueryRecords

---
+++ TraceDefense/TraceDefense.DAL/Repositories/CosmosDb/AzureStorageRepository.cs
@@ -0,0 +1,24 @@
+﻿using TraceDefense.DAL.Providers;
+
+namespace TraceDefense.DAL.Repositories.CosmosDb
+{
+    /// <summary>
+    /// CosmosDB Table Repository base class
+    /// </summary>
+    public abstract class AzureStorageRepository
+    {
+        /// <summary>
+        /// Storage Account Connection Factory object
+        /// </summary>
+        protected readonly AzureStorageConnectionFactory ConnectionFactory;
+
+        /// <summary>
+        /// Creates a new <see cref="AzureStorageRepository"/> instance
+        /// </summary>
+        /// <param name="connectionFactory">Storage account connection factory instance</param>
+        public AzureStorageRepository(AzureStorageConnectionFactory connectionFactory)
+        {
+            this.ConnectionFactory = connectionFactory;
+        }
+    }
+}
