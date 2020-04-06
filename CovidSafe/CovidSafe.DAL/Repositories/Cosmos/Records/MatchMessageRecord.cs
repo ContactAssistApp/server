67c2377c453d86f14301e0adc382156d22747cde
Cory Gehr <corygehr@outlook.com>
2020-04-06 12:06:36 -0700

    Update record version

--- CovidSafe/CovidSafe.DAL/Repositories/Cosmos/Records/MatchMessageRecord.cs
+++ CovidSafe/CovidSafe.DAL/Repositories/Cosmos/Records/MatchMessageRecord.cs
@@ -41,7 +41,7 @@ namespace CovidSafe.DAL.Repositories.Cosmos.Records
         /// Current version of record schema
         /// </summary>
         [JsonIgnore]
-        public const string CURRENT_RECORD_VERSION = "1.0.2";
+        public const string CURRENT_RECORD_VERSION = "1.0.3";
 
         /// <summary>
         /// Creates a new <see cref="MatchMessageRecord"/> instance
