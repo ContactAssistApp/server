be7f1aa8ee4296ff5e34b307393c05cb588fe909
Cory Gehr <corygehr@outlook.com>
2020-04-10 13:15:42 -0700

    Add API Tests project

---
+++ CovidSafe/CovidSafe.API.Tests/Controllers/MessageControllerTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CovidSafe.API.Tests.Controllers
+{
+    /// <summary>
+    /// Unit Tests for the <see cref="MessageController"/> class
+    /// </summary>
+    [TestClass]
+    public class MessageControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
