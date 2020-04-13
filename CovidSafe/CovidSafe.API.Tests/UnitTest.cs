3a75f241e3573a8cc98bef64fcfb74c5f9e97258
Kevin Gao <kevingao@microsoft.com>
2020-04-13 13:52:26 -0400

    API unit tests init

--- CovidSafe/CovidSafe.API.Tests/UnitTest.cs
+++ CovidSafe/CovidSafe.API.Tests/UnitTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using CovidSafe.API.Controllers;
+
 
 namespace CovidSafe.API.Tests
 {
@@ -9,5 +12,20 @@ namespace CovidSafe.API.Tests
         public void TestMethod()
         {
         }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
+        public void TestAddDynamicDataMethod(int a, int b, int expected)
+        {
+            var actual = a+b; //TODO: unit test functions under Controllers.MessageController
+            Assert.AreEqual(expected, actual);
+        }
+        public static IEnumerable<object[]> GetData()
+        {
+            yield return new object[] { 1, 1, 2 };
+            yield return new object[] { 12, 30, 42 };
+            yield return new object[] { 14, 1, 15 };
+        }
+
     }
 }
