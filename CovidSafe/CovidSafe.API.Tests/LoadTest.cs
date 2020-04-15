3890090f61fd6393db99b4c03709b26e155f2160
Kevin Gao <kevingao@microsoft.com>
2020-04-14 21:59:22 -0400

    add loading test -init

--- CovidSafe/CovidSafe.API.Tests/LoadTest.cs
+++ CovidSafe/CovidSafe.API.Tests/LoadTest.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Text;
+using CovidSafe.API.Controllers;
 
 namespace CovidSafe.API.Tests
 {
     class LoadTest
     {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+
+        [DataTestMethod]
+        [assembly: Parallelize(Workers = 0, Scope = ExecutionScope.MethodLevel)]
+        [DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
+        public void TestAddDynamicDataMethod(int a, int b, int expected)
+        {
+            var actual = a + b; //TODO: unit test functions under Controllers.MessageController
+            Assert.AreEqual(expected, actual);
+        }
+        public static IEnumerable<object[]> GetData()
+        {
+            yield return new object[] { 1, 1, 2 };
+            yield return new object[] { 12, 30, 42 };
+            yield return new object[] { 14, 1, 15 };
+        }
     }
 }
