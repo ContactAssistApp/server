bfc77d5470c02d3135758a611e0631130c7fd731
Alexey Taymanov <ataymano@microsoft.com>
2020-08-03 12:12:55 -0400

    basic auth

---
+++ CovidSafe/CovidSafe.DAL/Services/TestUserService.cs
@@ -0,0 +1,17 @@
+﻿using CovidSafe.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CovidSafe.DAL.Services
+{
+    public class TestUserService : IUserService
+    {
+        /// <inheritdoc/>
+        public async Task<User> Authenticate(string username, string password, CancellationToken cancellationToken)
+        {
+            if (username == "admin" && password == "password")
+                return new User { Username = username };
+            return null;
+        }
+    }
+}
